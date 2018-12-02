37021f57a73bea44cc8522da7b7c3c5b0e0d3de0
katietudinous <katiebarajas72@gmail.com>
2018-12-01 21:20:50 -0500

    Add files via upload

---
+++ torqueout.py
@@ -0,0 +1,73 @@
+"""
+Dyno Torque Sensor Torque Output Logging Script
+"""
+
+import sys
+from labjack import ljm
+import time
+from datetime import datetime
+import numpy as np
+import signal
+import csv
+
+def signal_handler(signal, frame):
+    global handle
+    print("\nStop Stream")
+    ljm.eStreamStop(handle)
+
+    # Close handle
+    ljm.close(handle)
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+MAX_REQUESTS = 50 # The number of eStreamRead calls that will be performed.
+
+# Open first found LabJack
+handle = ljm.openS("ANY", "ANY", "ANY")
+
+info = ljm.getHandleInfo(handle)
+print("Opened a LabJack with Device type: %i, Connection type: %i,\n" \
+    "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" % \
+    (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+
+try:
+    output_names = ["TIME", "VOLTAGE_1", "VOLTAGE_2", "TORQUE"]
+    start = datetime.now()
+
+    cur_log = ("cvttorque" + "_" + str(start.month) + "_" + str(start.day) + "_"+ str(start.year) + "_" + str(start.hour) + "_" + str(start.minute) + "_" + str(start.second) + ".csv")
+
+    with open(cur_log, "w") as f:
+        write = csv.writer(f)
+        write.writerow(output_names)
+
+    start = time.time()
+    while True:
+        ref = time.time() - start
+        voltage1 = ljm.eReadName(handle, "DIO0_EF_READ_A")
+        voltage2 = ljm.eReadName(handle, "DIO1_EF_READ_A")
+
+        #torque output is linear 5v to -5v
+        if (voltage1 > 0):
+            torque = voltage1*200
+        else:
+            torque = voltage2*200
+
+        output_values = [ref, voltage1, voltage2, torque]
+
+        print(output_values)
+
+        with open(cur_log,'a') as f:
+          write = csv.writer(f)
+          write.writerow(output_values)
+        time.sleep(20)
+
+    end = datetime.now()
+
+except ljm.LJMError:
+    ljme = sys.exc_info()[1]
+    print(ljme)
+except Exception:
+    e = sys.exc_info()[1]
+    print(e)
